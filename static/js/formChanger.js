fc3c6370020325f0426286726915b9b837a7b929
Johannes Filter <hi@jfilter.de>
2019-01-21 20:30:38 +0100

    trying to tame the navbar situation

--- static/js/formChanger.js
+++ static/js/formChanger.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
-  var $toggleHandler = $('input[data-toggle]');
-  $toggleHandler.on('change', function() {
-    var connect = $(this).data('toggle');
-    var $target = $('[data-toggle-target]');
+  var $toggleHandler = $("input[data-toggle]");
+  $toggleHandler.on("change", function() {
+    var connect = $(this).data("toggle");
+    var $target = $("[data-toggle-target]");
 
-    if (connect === 'show') {
-      $target.addClass('is-visible');
-      $("#birthdate").prop('required',true);
+    if (connect === "show") {
+      $target.addClass("is-visible");
+      $("#birthdate").prop("required", true);
     } else {
-      $target.removeClass('is-visible');
-      $("#birthdate").prop('required',false);
+      $target.removeClass("is-visible");
+      $("#birthdate").prop("required", false);
     }
   });
 });
